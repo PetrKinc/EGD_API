fd08011f2780af5d0d2fad06da6a89954a9dc7dd
petr_kinc <petr.k.kinc@pwc.com>
2023-04-25 17:10:09 +0200

    add new controller

---
+++ EGD_API/Controllers/ListenerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EGD_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListenerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string message)
+        {
+            // Process the request and return a response
+            return Ok($"Received message: {message}");
+        }
+    }
+}
